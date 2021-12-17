62777ea0299fc5e50867c633c573918756214f25
margielewski <stanislaw.margielewski@gmail.com>
2021-12-17 19:54:10 +0100

    fix: change themetype import path

--- libs/ui/src/types/definitions/styled.d.ts
+++ libs/ui/src/types/definitions/styled.d.ts
@@ -1,6 +1,7 @@
-import 'styled-components';
-import { ThemeType } from './CommonTypes';
+import {} from 'styled-components';
+import { ThemeType } from '../CommonTypes';
 
 declare module 'styled-components' {
-  export type DefaultTheme = ThemeType;
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends ThemeType {}
 }
