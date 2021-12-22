96cf73277b88d7f08a0db98be397abb556ee3a05
amargielewski <aleksander.margielewski4@gmail.com>
2021-12-22 13:24:56 +0100

    feat: Change Checkbox Component

--- libs/ui/src/lib/Checkbox/Checkbox.stories.tsx
+++ libs/ui/src/lib/Checkbox/Checkbox.stories.tsx
@@ -10,6 +10,3 @@ const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
-
-export const Secondary = Template.bind({});
-Secondary.args = {};
--- libs/ui/src/lib/Checkbox/Checkbox.tsx
+++ libs/ui/src/lib/Checkbox/Checkbox.tsx
@@ -1,60 +1,48 @@
 import { useState } from 'react';
 import styled, { css } from 'styled-components';
-import { Icon, IconProps } from '../Icon/Icon';
 import { InputHTMLAttributes } from 'react';
+import CheckedIcon from '../../Assets/CheckedIcon.svg';
+
 export type CheckboxProps = InputHTMLAttributes<HTMLInputElement>;
 
-const StyledCheckboxContainer = styled.div`
-  display: inline-block;
-  vertical-align: middle;
-`;
 const StyledInput = styled.input`
-  border: 0;
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-`;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: transparent;
+  margin: 0;
 
-const StyledUnchecked = css`
-  border: 1px solid ${({ theme }) => theme.colors.checkbox_border};
-`;
-
-const StyledCheckbox = styled.div<{
-  checked: boolean;
-}>`
-  display: inline-block;
   width: 20px;
   height: 20px;
-  background: ${({ checked, theme }) =>
-    checked ? theme.colors.checkbox_checked : 'transparent'};
+  color: ${({ theme }) => theme.colors.text_primary};
+  border: 1px solid ${({ theme }) => theme.colors.checkbox_border};
   border-radius: ${({ theme }) => theme.borderRadius.checkbox_default}px;
-  transition: all 150ms;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  ${({ checked }) => !checked && StyledUnchecked}
+
+  display: grid;
+  place-content: center;
+
+  ::before {
+    content: '';
+    width: 20px;
+    height: 20px;
+    transform: scale(0);
+    transition: 0.12s transform ease-in-out;
+    background-color: ${({ theme }) => theme.colors.checkbox_checked};
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    border-radius: ${({ theme }) => theme.borderRadius.checkbox_default}px;
+    background-image: url(${CheckedIcon});
+    transform-origin: top left;
+  }
+  :checked::before {
+    transform: scale(1);
+  }
+
+  :disabled {
+    background-color: ${({ theme }) => theme.colors.checkbox_border};
+    cursor: not-allowed;
+  }
 `;
 
-export const Checkbox = (): CheckboxProps => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  return (
-    <StyledCheckboxContainer>
-      <StyledInput type="checkbox" />
-      <StyledCheckbox
-        onClick={() => {
-          if (isChecked) setIsChecked(false);
-          if (!isChecked) setIsChecked(true);
-        }}
-      >
-        {isChecked && <Icon name="CheckboxArrowIcon" size={15} />}
-      </StyledCheckbox>
-    </StyledCheckboxContainer>
-  );
+export const Checkbox = (props: CheckboxProps) => {
+  return <StyledInput type="checkbox" disabled />;
 };
